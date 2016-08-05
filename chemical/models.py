dbfc5fe0524f5cbea19adc66c6244925a5744239
Bill Monsueir <bmonsueir@gmail.com>
2016-08-05 20:50:38 +0000

    initial

---
+++ chemical/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Chemical(models.Model):
+    name = models.CharField( max_length=255,)
+    value = models.CharField( max_length=255,)
+    attribute = models.CharField(max_length=255,)
+    updatedBy = models.CharField(max_length=255,)
+    references = models.CharField( max_length=255,)
+    permissions = models.CharField(max_length=255,)
+    updatedAt = models.CharField( max_length=255,)
+
